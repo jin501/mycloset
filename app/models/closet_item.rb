97753b632e4df797b303b1d801cd514da0645741
jin501 <soljin01@gmail.com>
2016-11-14 10:13:12 -0500

    users model

--- app/models/closet_item.rb
+++ app/models/closet_item.rb
@@ -2,6 +2,6 @@ class ClosetItem < ApplicationRecord
   belongs_to :item
   belongs_to :closet
   has_many :tags
-  belongs_to :user
+  has_one :user, through: :closet
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :closets
   has_many :items, through: :closets
-  has_many :closet_items
+  has_many :closet_items, through: :closets 
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
